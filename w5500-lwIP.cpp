76ce549366037609782299d6d53a6106921af4ad
Jean Charles Mammana <jcmammana@scallog.com>
2018-06-13 09:22:56 +0200

    Fix compilation error for ESP32 toolchain based on the last master changes.

--- w5500-lwIP.cpp
+++ w5500-lwIP.cpp
@@ -1,5 +1,7 @@
 
-// TODO: remove all Serial.print
+// TODO:
+// remove all Serial.print
+// unchain pbufs
 
 #include <IPAddress.h>
 
@@ -8,19 +10,18 @@
 #include <lwip/dhcp.h>
 
 #ifdef ESP8266
-#include <NetDump.h>
-#include <user_interface.h>
+#include <user_interface.h>	// wifi_get_macaddr()
 #endif
 
 #include "w5500-lwIP.h"
 
-boolean Wiznet5500lwIP::begin(SPIClass &spi, const uint8_t* macAddress, uint16_t mtu)
+boolean Wiznet5500lwIP::begin (SPIparam(SPIClass& spi,) const uint8_t* macAddress, uint16_t mtu)
 {
     uint8_t zeros[6] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
     if (!macAddress)
         macAddress = zeros;
         
-    if (!Wiznet5500::begin(spi, macAddress))
+    if (!Wiznet5500::begin(SPIparam(spi,) macAddress))
         return false;
     _mtu = mtu;
 
@@ -38,27 +39,6 @@ boolean Wiznet5500lwIP::begin(SPIClass &spi, const uint8_t* macAddress, uint16_t
     }
 }
 
-/**
-* @ingroup lwip_nosys
-* Forwards a received packet for input processing with
-* ethernet_input() or ip_input() depending on netif flags.
-* Don't call directly, pass to netif_add() and call
-* netif->input().
-* Only works if the netif driver correctly sets
-* NETIF_FLAG_ETHARP and/or NETIF_FLAG_ETHERNET flag!
-*/
-err_t
-netif_input(struct pbuf *p, struct netif *inp)
-{
-#if LWIP_ETHERNET
-  if (inp->flags & (NETIF_FLAG_ETHARP | NETIF_FLAG_ETHERNET)) {
-    return ethernet_input(p, inp);
-  }
-  else
-#endif /* LWIP_ETHERNET */
-    return ip_input(p, inp);
-}
-
 err_t Wiznet5500lwIP::start_with_dhclient ()
 {
     ip4_addr_t ip, mask, gw;
@@ -70,7 +50,7 @@ err_t Wiznet5500lwIP::start_with_dhclient ()
     _netif.hwaddr_len = sizeof _mac_address;
     memcpy(_netif.hwaddr, _mac_address, sizeof _mac_address);
     
-    if (!netif_add(&_netif, &ip, &mask, &gw, this, netif_init_s, ethernet_input))// netif_input))
+    if (!netif_add(&_netif, &ip, &mask, &gw, this, netif_init_s, ethernet_input))
         return ERR_IF;
 
     _netif.flags |= NETIF_FLAG_UP;
@@ -82,39 +62,19 @@ err_t Wiznet5500lwIP::linkoutput_s (netif *netif, struct pbuf *pbuf)
 {
     Wiznet5500lwIP* ths = (Wiznet5500lwIP*)netif->state;
     
-    /*
+#ifdef ESP8266
     if (pbuf->len != pbuf->tot_len || pbuf->next)
         Serial.println("ERRTOT\r\n");
+#endif
 
     uint16_t len = ths->sendFrame((const uint8_t*)pbuf->payload, pbuf->len);
-    */
 
-    struct pbuf *p = pbuf;
-
-    //if (p->len != p->tot_len)
-    //  Serial.printf("Chained : p->len=%d p->tot_len=%d\r\n", p->len, p->tot_len);
-
-    do 
-    {
-      uint16_t len = ths->sendFrame((const uint8_t*)p->payload, p->len);
-      if (len != p->len)
-      {
-        Serial.printf("SendFrame() : wrong buffer length p->len=%d, returned=%d\r\n", p->len, len);
-        //return ERR_MEM;
-      }
-
-      p = pbuf->next;
-    } while (p);
-    
-    return ERR_OK;
-
-
-#ifdef ESP8266
+#if defined(ESP8266) && PHY_HAS_CAPTURE
     if (phy_capture)
         phy_capture(ths->_netif.num, (const char*)pbuf->payload, pbuf->len, /*out*/1, /*success*/len == pbuf->len);
 #endif
     
-    //return len == pbuf->len? ERR_OK: ERR_MEM;
+    return len == pbuf->len? ERR_OK: ERR_MEM;
 }
 
 err_t Wiznet5500lwIP::netif_init_s (struct netif* netif)
@@ -128,9 +88,18 @@ err_t Wiznet5500lwIP::netif_init ()
     if (memcmp(_mac_address, zeros, 6) == 0)
     {
 #ifdef ESP8266
-        if (!wifi_get_macaddr(STATION_IF, (uint8*)zeros))
-            Serial.println("can u see me?");
-        // I understand this is cheating
+
+        // make a new mac-address from the esp's wifi sta one
+        // I understand this is cheating with an official mac-address
+        wifi_get_macaddr(STATION_IF, (uint8*)zeros);
+
+#else
+
+        // https://serverfault.com/questions/40712/what-range-of-mac-addresses-can-i-safely-use-for-my-virtual-machines
+        // TODO api to get a mac-address from user
+        // TODO ESP32: get wifi mac address like with esp8266 above
+        zeros[0] = 0xEE;
+
 #endif
         zeros[3] += _netif.num;
         memcpy(_mac_address, zeros, 6);
@@ -147,7 +116,13 @@ err_t Wiznet5500lwIP::netif_init ()
         | NETIF_FLAG_BROADCAST 
         | NETIF_FLAG_LINK_UP;
 
+    // lwIP's doc: This function typically first resolves the hardware
+    // address, then sends the packet.  For ethernet physical layer, this is
+    // usually lwIP's etharp_output()
     _netif.output = etharp_output;
+    
+    // lwIP's doc: This function outputs the pbuf as-is on the link medium
+    // (this must points to the raw ethernet driver, meaning: us)
     _netif.linkoutput = linkoutput_s;
     
     return ERR_OK;
@@ -159,80 +134,47 @@ err_t Wiznet5500lwIP::loop ()
     if (!tot_len)
         return ERR_OK;
     
-    struct pbuf *pbuf = pbuf_alloc(PBUF_RAW, tot_len, PBUF_POOL);
-    if (!pbuf)
+    // from doc: use PBUF_RAM for TX, PBUF_POOL from RX
+    // however:
+    // PBUF_POOL can return chained pbuf (not in one piece)
+    // and WiznetDriver does not have the proper API to deal with that
+    // so in the meantime, we use PBUF_RAM instead which are currently
+    // guarantying to deliver a continuous chunk of memory.
+    // TODO: tweak the wiznet driver to allow copying partial chunk
+    //       of received data and use PBUF_POOL.
+    pbuf* pbuf = pbuf_alloc(PBUF_RAW, tot_len, PBUF_RAM);
+    if (!pbuf || pbuf->len < tot_len)
     {
-      Serial.printf("pbuf_alloc() : Error tot_len=%d\r\n", tot_len);
-      discardFrame(tot_len);
-      return ERR_BUF;
-    }
-
-    struct pbuf *p = pbuf;
-    uint16_t l = tot_len;
-
-    //if (p->len != p->tot_len)
-    //  Serial.printf("Chained : p->len=%d p->tot_len=%d\r\n", p->len, p->tot_len);
-
-    do 
-    {
-      uint16_t len = readFrameData((uint8_t*)p->payload, p->len);
-
-      if (len != p->len)
-      {
-        if (len > 0)
-          Serial.printf("readFrameData() : wrong buffer length p->len=%d, returned=%d\r\n", p->len, len);
-
-        discardFrame(tot_len);
-        pbuf_free(pbuf);
-
-        return ERR_BUF;
-      }
-
-      l = l - len;
-      p = p->next;
-    } while (p && l > 0);
-    
-    err_t err = _netif.input(pbuf, &_netif);
-
-    if (err != ERR_OK)
-      pbuf_free(pbuf);
-
-    return err;
-
-
-
-    /*
-    if (pbuf->next || pbuf->tot_len != pbuf->len)
-    {
-        Serial.println("CHAINED?\r\n");
+        if (pbuf)
+            pbuf_free(pbuf);
         discardFrame(tot_len);
         return ERR_BUF;
     }
 
     uint16_t len = readFrameData((uint8_t*)pbuf->payload, tot_len);
-    if (!len)
+    if (len != tot_len)
     {
+        // tot_len is given by readFrameSize()
+        // and is supposed to be honoured by readFrameData()
+        // todo: ensure this test is unneeded, remove the print
         Serial.println("read error?\r\n");
+        pbuf_free(pbuf);
         return ERR_BUF;
-    }
-
-    if (pbuf->len != tot_len)
-    {
-        Serial.println("pbuf too large?\r\n");
-        pbuf->len = tot_len;
     }
 
     err_t err = _netif.input(pbuf, &_netif);
 
-#ifdef ESP8266
+#if defined(ESP8266) && PHY_HAS_CAPTURE
     if (phy_capture)
-        phy_capture(_netif.num, (const char*)pbuf->payload, tot_len, 0, err == ERR_OK);
+        phy_capture(_netif.num, (const char*)pbuf->payload, tot_len, /*out*/0, /*success*/err == ERR_OK);
 #endif
 
     if (err != ERR_OK)
+    {
         pbuf_free(pbuf);
+        return err;
+    }
 
-    return err;
-
-    */
+    // allocated pbuf is now caller's responsibility
+    return ERR_OK;
 }
