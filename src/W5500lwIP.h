3cdf6afffb6df024dc7fe99f7b2d214bb9128f59
david gauchard <gauchard@laas.fr>
2019-05-23 22:22:28 +0200

    add missing file

---
+++ src/W5500lwIP.h
@@ -0,0 +1,10 @@
+
+#ifndef _W5500LWIP_H
+#define _W5500LWIP_H
+
+#include <utility/lwIPeth.h>
+#include <utility/w5500.h>
+
+using Wiznet5500lwIP = LwipEthernet<Wiznet5500>;
+
+#endif // _W5500LWIP_H
