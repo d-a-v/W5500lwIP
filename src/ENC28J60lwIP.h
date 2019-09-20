6a8c5771d81edcd4124b5eeb7a82753672974498
david gauchard <gauchard@laas.fr>
2019-09-21 00:30:05 +0200

    enc28j60 support

---
+++ src/ENC28J60lwIP.h
@@ -0,0 +1,10 @@
+
+#ifndef _ENC28J60LWIP_H
+#define _ENC28J60LWIP_H
+
+#include <utility/lwIPeth.h>
+#include <utility/enc28j60.h>
+
+using ENC28J60lwIP = LwipEthernet<ENC28J60>;
+
+#endif // _ENC28J60LWIP_H
